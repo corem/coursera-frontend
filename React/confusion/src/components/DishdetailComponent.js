711f0fa63030f1c29ec282d04410ef5810d3fc96
Remi Cornet <cornet.remi@gmail.com>
2019-12-22 16:33:38 +0100

    Refactoring into functional components

---
+++ React/confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+
+function RenderDish({ dish }) {
+    if (dish != null) {
+        return (
+            <div className='col-12 col-md-5 m-1'>
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+    else {
+        return (<div></div>)
+    }
+}
+
+function RenderComments({ comments }) {
+    const com = comments.map(comment => {
+        return (
+            <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>-- {comment.author},&nbsp;
+                    {new Intl.DateTimeFormat('en-US', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                }).format(new Date(comment.date))}
+                </p>
+            </li>
+        )
+    })
+    return (
+        <div className='col-12 col-md-5 m-1'>
+            <h4> Comments </h4>
+            <ul className='list-unstyled'>
+                {com}
+            </ul>
+
+        </div>
+    )
+}
+
+const DishDetail = (props) => {
+    if (props.dish != null) {
+        return (
+            <div className='row'>
+                <RenderDish dish={props.dish} />
+                <RenderComments comments={props.dish.comments} />
+            </div>
+        )
+    } else {
+        return (<div></div>)
+    }
+}
+
+export default DishDetail
