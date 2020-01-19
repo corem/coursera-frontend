1a8b45cfeab395950db8b20f60bb2cf20bf9b2ed
Remi Cornet <cornet.remi@gmail.com>
2020-01-19 15:48:27 +0100

    Assignment 4, task 1

--- React/confusion/src/components/AboutComponent.js
+++ React/confusion/src/components/AboutComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function About(props) {
 
@@ -9,7 +11,7 @@ function About(props) {
             <div className="col-md-12 mt-5">
                 <Media tag="li">
                     <Media left middle>
-                        <Media object src={leader.image} alt={leader.name} />
+                        <Media object src={baseUrl + leader.image} alt={leader.name} />
                     </Media>
                     <Media body className="ml-5">
                         <Media heading>{leader.name}</Media>
@@ -21,7 +23,22 @@ function About(props) {
         );
     }
 
-    const leaders = props.leaders.map((leader) => {
+    function RenderLeaders(){
+
+        if (props.leaders.isLoading) {
+            return <Loading />;
+        }
+        else if (props.leaders.errMess) {
+            return (
+                <h4>{props.leaders.errMess}</h4>
+            );
+        }
+        else return (
+            <RenderLeaders />
+        );
+    }
+
+    const leaders = props.leaders.leaders.map(leader => {
         return (
             <div className="col-12 col-md-12 m-1">
                 <RenderLeader leader={leader} />
